b091a41e43c56eac389e47265b8089c3ec2abcb8
LukZe21 <ldzotsenidze4@gmail.com>
2024-12-15 20:41:21 +0400

    second commit

--- solver.cpp
+++ solver.cpp
@@ -12,6 +12,10 @@ int main(){
                                             {"r5c0","r5c1","r5c2","r5c3","r5c4","r5c5","r5c6","r5c7"},
                                             {"r6c0","r6c1","r6c2","r6c3","r6c4","r6c5","r6c6","r6c7"},
                                             {"r7c0","r7c1","r7c2","r7c3","r7c4","r7c5","r7c6","r7c7"}};
+
+    vector<vector<string>> possibleBlocks = {{"r2c3", "r3c3", "r3c2", "r3c4"}};
+    vector<vector<int>> possibleBlocksInt;
+    vector<vector<string>> blastTemplateCopy;
     while(true){
         string inpt;
         std::cout << "Specify occupied slots >> ";
@@ -19,9 +23,9 @@ int main(){
         size_t ridx = 0;
         if(inpt == "exit")
             break;
-        for(auto row : blastTemplate){
+        for(auto &row : blastTemplate){
             size_t cidx = 0;
-            for(auto col : row){
+            for(auto &col : row){
                 if(col == inpt){
                     blastTemplate[ridx][cidx] += 'o';
                 }
@@ -30,12 +34,82 @@ int main(){
             ridx++;
         }
     }
+    for(const auto &block : possibleBlocks){
+        for(const auto &val : block){
+            int valRowNum = val[1] - '0'; // get number value of block table row
+            int valColNum = val[3] - '0'; // get number value of block table row
+            possibleBlocksInt.push_back({valRowNum, valColNum});
+        }
+    }
+    vector<int> previousVector = {-1, -1};
+    size_t dridx;
+    size_t dcidx;
+    for(const auto &blockVector : possibleBlocksInt){
+        for(size_t ridx=0; ridx<8; ++ridx){
+            for(size_t cidx=0; cidx<8; ++cidx){
+                const auto &val = blastTemplate[ridx][cidx];
+                if(val.find('o') != std::string::npos || val.find('p') != std::string::npos){
+                    continue;
+                }
+                if(blastTemplateCopy.empty())
+                    blastTemplateCopy.assign(blastTemplate.begin(), blastTemplate.end());
+                
+                // possible positions
+                if((blockVector[0] > previousVector[0] ) && (blockVector[0] > previousVector[0])){
+                    dridx = ridx+1;
+                    dcidx = cidx+1;
+                }
+                else if((blockVector[0] < previousVector[0] ) && (blockVector[0] > previousVector[0])){
+                    dridx = ridx-1;
+                    dcidx = cidx+1;
+                }
+                else if((blockVector[0] < previousVector[0] ) && (blockVector[0] < previousVector[0])){
+                    dridx = ridx-1;
+                    dcidx = cidx-1;
+                }
+                else if((blockVector[0] > previousVector[0] ) && (blockVector[0] < previousVector[0])){
+                    dridx = ridx+1;
+                    dcidx = cidx-1;
+                }
+                else if((blockVector[0] == previousVector[0] ) && (blockVector[0] > previousVector[0])){
+                    dridx = ridx;
+                    dcidx = cidx+1;
+                }
+                else if((blockVector[0] == previousVector[0] ) && (blockVector[0] < previousVector[0])){
+                    dridx = ridx;
+                    dcidx = cidx-1;
+                }
+                else if((blockVector[0] > previousVector[0] ) && (blockVector[0] == previousVector[0])){
+                    dridx = ridx+1;
+                    dcidx = cidx;
+                }
+                else if((blockVector[0] < previousVector[0] ) && (blockVector[0] == previousVector[0])){
+                    dridx = ridx-1;
+                    dcidx = cidx;
+                }
+                
+                // appending
+                if (dridx < blastTemplate.size() && dcidx < blastTemplate[dridx].size()){
+                    if((blastTemplate[dridx][dcidx].find('p') == std::string::npos) && (blastTemplate[dridx][dcidx].find('o') == std::string::npos))
+                        blastTemplate[dridx][dcidx] += 'p';
+                }
+            }
+        }
+        previousVector = blockVector;
+    }        
+
+    // for(auto vector : possibleBlocksInt){
+    //     for(auto val : vector)
+    //         cout << val << ", ";
+    //     cout << endl;
+    // }
 
     for(auto row : blastTemplate){
         for(auto val : row)
-            std::cout << val << ' ';
-        std::cout << '\n';
+            cout << val << ' ';
+        cout << '\n';
     }
 
     return 0;
 }
+
