af31dba9a9e03170266c649c17fceb7e3a002ae6
LukZe21 <ldzotsenidze4@gmail.com>
2024-12-18 19:32:09 +0400

    eleventh commit

--- solver.cpp
+++ solver.cpp
@@ -9,18 +9,19 @@ vector<vector<vector<int>>> vstr_to_vint(vector<vector<string>> possibleBlocks);
 vector<vector<vector<int>>> distance_vector(vector<vector<vector<int>>> possibleBlocksInt);
 
 int main(){
-    vector<vector<string>> blastTemplate = {{"r0c0o","r0c1","r0c2","r0c3","r0c4","r0c5","r0c6","r0c7"},
-                                            {"r1c0o","r1c1","r1c2","r1c3","r1c4","r1c5","r1c6","r1c7"},
-                                            {"r2c0o","r2c1","r2c2","r2c3","r2c4","r2c5","r2c6","r2c7"},
-                                            {"r3c0o","r3c1","r3c2","r3c3","r3c4","r3c5","r3c6","r3c7"},
-                                            {"r4c0o","r4c1","r4c2","r4c3","r4c4","r4c5","r4c6","r4c7"},
+    vector<vector<string>> blastTemplate = {{"r0c0","r0c1","r0c2","r0c3","r0c4","r0c5","r0c6","r0c7"},
+                                            {"r1c0","r1c1","r1c2","r1c3","r1c4","r1c5","r1c6","r1c7"},
+                                            {"r2c0","r2c1","r2c2","r2c3","r2c4","r2c5","r2c6","r2c7"},
+                                            {"r3c0","r3c1","r3c2","r3c3","r3c4","r3c5","r3c6","r3c7"},
+                                            {"r4c0","r4c1","r4c2","r4c3","r4c4","r4c5","r4c6","r4c7"},
                                             {"r5c0","r5c1","r5c2","r5c3","r5c4","r5c5","r5c6","r5c7"},
-                                            {"r6c0o","r6c1","r6c2o","r6c3o","r6c4o","r6c5o","r6c6o","r6c7o"},
-                                            {"r7c0o","r7c1","r7c2o","r7c3o","r7c4o","r7c5o","r7c6o","r7c7o"}};
+                                            {"r6c0","r6c1o","r6c2o","r6c3o","r6c4o","r6c5o","r6c6o","r6c7o"},
+                                            {"r7c0","r7c1o","r7c2o","r7c3o","r7c4o","r7c5o","r7c6o","r7c7o"}};
 
-    vector<vector<string>> possibleBlocks = {{"r7c1", "r6c1", "r5c1", "r5c0"}};
+    vector<vector<string>> possibleBlocks = {{"r7c0", "r6c0", "r6c1", "r7c1"}, {"r7c2", "r7c3", "r7c4"}, {"r7c6", "r6c6"}};
     vector<vector<vector<int>>> possibleBlocksInt = vstr_to_vint(possibleBlocks);
     vector<vector<string>> blastTemplateCopy;
+    vector<vector<string>> blastTemplateTemp;
     vector<vector<string>> bestTemplate;
     int possibleBlocksSize = 0;
     for(auto vector : possibleBlocks){
@@ -47,53 +48,56 @@ int main(){
         }
     }
 
-    vector<vector<vector<int>>>rslt = distance_vector(possibleBlocksInt);
-    
-    // fix the problem with when the blocks are not first, the unoccupied edges don't get occupied by blocks
+    // make an algorithm that adds block(s) to the table and iterates over other block(s) to find the best possible combination of blocks
+
     int prevBestPoint = 0;
-    for(auto &block : rslt){
-        vector<vector<int>> freeSlots = blastFreeSlots(blastTemplate);
-        for(auto &freeVec : freeSlots){
-            blastTemplateCopy.assign(blastTemplate.begin(), blastTemplate.end());
-            bool err = false;
-            int point = 0;            
-            for(auto &bvec : block){
-                if((freeVec[0]+bvec[0] >= 0 && freeVec[0]+bvec[0] < 8) && (freeVec[1]+bvec[1] >= 0 && freeVec[1]+bvec[1] < 8)){
-                    if(blastTemplateCopy[freeVec[0]+bvec[0]][freeVec[1]+bvec[1]].find('o') == string::npos)
-                        blastTemplateCopy[freeVec[0]+bvec[0]][freeVec[1]+bvec[1]] += 'o';
-                    else
-                        err = true;
-                } else
-                    err = true;
-            }
-            if(!err){
-                for(int r=0; r<8;r++){
-                    bool allContainO = true;
-                    for(int c=0; c<8;c++)
-                        if(blastTemplateCopy[r][c].find('o') == string::npos){
-                            allContainO = false;
-                            break;
-                        }
-                    if(allContainO){
-                        point++;
+    for(auto &blocks : possibleBlocksInt){
+       vector<vector<vector<int>>> rslt = distance_vector({blocks});
+        for(auto &block : rslt){
+            vector<vector<int>> freeSlots = blastFreeSlots(blastTemplate);
+            for(auto &freeVec : freeSlots){
+                blastTemplateCopy.assign(blastTemplate.begin(), blastTemplate.end());
+                bool err = false;
+                for(auto &bvec : block){
+                    if((freeVec[0]+bvec[0] >= 0 && freeVec[0]+bvec[0] < 8) && (freeVec[1]+bvec[1] >= 0 && freeVec[1]+bvec[1] < 8)){
+                        if(blastTemplateCopy[freeVec[0]+bvec[0]][freeVec[1]+bvec[1]].find('o') == string::npos)
+                            blastTemplateCopy[freeVec[0]+bvec[0]][freeVec[1]+bvec[1]] += 'o';
+                        else
+                            err = true;                        
+                    } else{
+                        err = true;                 
                     }
                 }
-                for(int c=0; c<8;c++){
-                    bool allContainO = true;
-                    for(int r=0; r<8;r++)
-                        if(blastTemplateCopy[r][c].find('o') == string::npos){
-                            allContainO = false;
-                            break;
+                if(!err){
+                    int point = 0;
+                    for(int r=0; r<8;r++){
+                        bool allContainO = true;
+                        for(int c=0; c<8;c++)
+                            if(blastTemplateCopy[r][c].find('o') == string::npos){
+                                allContainO = false;
+                                break;
+                            }
+                        if(allContainO){
+                            point++;
                         }
-                    if(allContainO)
-                        point++;
-                }
-                if(point>prevBestPoint){
-                    prevBestPoint = point;
-                        bestTemplate.assign(blastTemplateCopy.begin(), blastTemplateCopy.end());
+                    }
+                    for(int c=0; c<8;c++){
+                        bool allContainO = true;
+                        for(int r=0; r<8;r++)
+                            if(blastTemplateCopy[r][c].find('o') == string::npos){
+                                allContainO = false;
+                                break;
+                            }
+                        if(allContainO)
+                            point++;
+                    }
+                    if(point>prevBestPoint){
+                        prevBestPoint = point;
+                            bestTemplate.assign(blastTemplateCopy.begin(), blastTemplateCopy.end());
+                    }
                 }
             }
-        }
+        }       
     }
 
     for(auto &row : bestTemplate){
